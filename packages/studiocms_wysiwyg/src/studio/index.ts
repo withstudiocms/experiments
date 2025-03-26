ca0e927e89de00dc85f1e3b7dbce5659f547b646
Adam Matthiesen <amatthiesen@outlook.com>
2025-03-26 09:10:16 -0700

    feat: restructure WYSIWYG plugin exports and add new renderer components

---
+++ packages/studiocms_wysiwyg/src/studio/index.ts
@@ -0,0 +1,42 @@
+/**
+ * These triple-slash directives defines dependencies to various declaration files that will be
+ * loaded when a user imports the StudioCMS plugin in their Astro configuration file. These
+ * directives must be first at the top of the file and can only be preceded by this comment.
+ */
+/// <reference types="./virtual.d.ts" preserve="true" />
+/// <reference types="studiocms/v/types" />
+
+import { addVirtualImports, createResolver } from 'astro-integration-kit';
+import { type StudioCMSPlugin, definePlugin } from 'studiocms/plugins';
+
+export function studiocmsWYSIWYG(): StudioCMSPlugin {
+	// Resolve the path to the current file
+	const { resolve } = createResolver(import.meta.url);
+
+	// Define the package identifier
+	const packageIdentifier = '@studiocms/wysiwyg/studio';
+
+	// Return the plugin configuration
+	return definePlugin({
+		identifier: packageIdentifier,
+		name: 'StudioCMS WYSIWYG Studio',
+		studiocmsMinimumVersion: '0.1.0-beta.13',
+		pageTypes: [
+			{
+				identifier: 'studiocms/studio',
+				label: 'GrapesJS StudioSDK',
+				rendererComponent: 'studiocms/html',
+				pageContentComponent: 'studiocms/html',
+			},
+		],
+		integration: {
+			name: packageIdentifier,
+			hooks: {
+				'astro:config:setup': () => {},
+				'astro:config:done': () => {},
+			},
+		},
+	});
+}
+
+export default studiocmsWYSIWYG;
---
+++ packages/studiocms_wysiwyg/src/studio/virtual.d.ts
@@ -0,0 +1,3 @@
+declare module 'virtual:studiocms/plugins/renderers' {
+	export const studiocms_studio: typeof import('./components/Render.astro').default;
+}
