883d60bb05067fcd237607ba4613285af7b96c47
Adam Matthiesen <amatthiesen@outlook.com>
2025-03-26 08:43:55 -0700

    feat: update WYSIWYG plugin configuration and rename export function

--- packages/studiocms_wysiwyg/src/index.ts
+++ packages/studiocms_wysiwyg/src/index.ts
@@ -8,27 +8,27 @@
 import { addVirtualImports, createResolver } from 'astro-integration-kit';
 import { type StudioCMSPlugin, definePlugin } from 'studiocms/plugins';
 
-export function plugin(): StudioCMSPlugin {
+export function studiocmsWYSIWYG(): StudioCMSPlugin {
 	// Resolve the path to the current file
 	const { resolve } = createResolver(import.meta.url);
 
 	// Define the package identifier
-	const packageIdentifier = '@studiocms/mdx';
+	const packageIdentifier = '@studiocms/wysiwyg';
 
 	// Return the plugin configuration
 	return definePlugin({
 		identifier: packageIdentifier,
-		name: 'StudioCMS plugin',
+		name: 'StudioCMS WYSIWYG Editor',
 		studiocmsMinimumVersion: '0.1.0-beta.13',
 		pageTypes: [],
 		integration: {
 			name: packageIdentifier,
 			hooks: {
-				'astro:config:setup': (params) => {},
+				'astro:config:setup': () => {},
 				'astro:config:done': () => {},
 			},
 		},
 	});
 }
 
-export default plugin;
+export default studiocmsWYSIWYG;
