f275b09cefdaad93bac482fce4f6813f6386c9d1
Adam Matthiesen <amatthiesen@outlook.com>
2025-04-07 04:23:03 -0700

    Setup Twitter posting API

--- packages/studiocms_socialposter/src/routes/postToTwitter.ts
+++ packages/studiocms_socialposter/src/routes/postToTwitter.ts
@@ -7,3 +7,40 @@ import {
 import logger from 'studiocms:logger';
 import { response } from '../utils/response.js';
 import type { APIContext, APIRoute } from 'astro';
+import { TwitterApi } from 'twitter-api-v2';
+
+export const POST: APIRoute = async (context: APIContext) => {
+	if (!TWITTER_API_KEY || !TWITTER_API_SECRET || !TWITTER_ACCESS_TOKEN || !TWITTER_ACCESS_SECRET) {
+		return response(500, JSON.stringify({ error: 'Missing ENV Variables' }));
+	}
+
+	// Initialize the Twitter client with credentials from environment variables
+	const client = new TwitterApi({
+		appKey: TWITTER_API_KEY,
+		appSecret: TWITTER_API_SECRET,
+		accessToken: TWITTER_ACCESS_TOKEN,
+		accessSecret: TWITTER_ACCESS_SECRET,
+	});
+
+	const { content } = await context.request.json();
+
+	if (!content) {
+		return response(500, JSON.stringify({ error: 'Content must not be empty' }));
+	}
+
+	try {
+		// Create the tweet
+		await client.v2.tweet(content);
+
+		return response(200, JSON.stringify({ message: 'Successfully sent Twitter/X message' }));
+	} catch (e) {
+		logger.error(`Error posting to Twitter / X: ${e}`);
+
+		return response(
+			500,
+			JSON.stringify({
+				error: (e as Error).message || 'Failed to post to Twitter / X',
+			})
+		);
+	}
+};
