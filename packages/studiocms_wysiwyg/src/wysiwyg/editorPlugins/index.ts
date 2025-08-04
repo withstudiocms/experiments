d36d1606bfd2f632061b76b136fea6a6a233d30e
Adam Matthiesen <amatthiesen@outlook.com>
2025-08-03 17:04:15 -0700

    Delete packages/studiocms_wysiwyg directory

--- packages/studiocms_wysiwyg/src/wysiwyg/editorPlugins/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as blocks } from './blocks/index.js';
-export { default as customCode } from './code/index.js';
-export { default as forms } from './forms/index.js';
-export { default as rte } from './rte/index.js';
-export { default as tabs } from './tabs/index.js';
-export { default as typed } from './typed/index.js';
-export { default as countdown } from './countdown.js';
-export { default as tooltip } from './tooltip.js';
-export { default as tuiImageEditor } from './tuiImageEditor.js';
--- packages/studiocms_wysiwyg/src/wysiwyg/virtual.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'virtual:studiocms/plugins/renderers' {
-	export const studiocms_wysiwyg: typeof import('./components/Render.astro').default;
-}
